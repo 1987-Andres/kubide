540c91f8af7cf03e0f3fde0bd693c640ecccbb7a
Mella <melladjs@hotmail.com>
2022-01-25 20:18:11 +0100

    Configurada conexion a BBDD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3006,
+      username: 'andres',
+      password: 'kubide',
+      autoLoadEntities: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
